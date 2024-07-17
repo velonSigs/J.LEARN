97d24f44bdeab293178f7802d051b9f3f42f82f6
velon <wu_velon@163.com>
2024-07-17 23:24:03 +0800

    0717上课

--- cpp/src/helloworld.cpp
+++ cpp/src/helloworld.cpp
@@ -4,5 +4,7 @@ int main(){
 cout << "helloworld" << endl;
 int a = 3;
 int b = 4;
-a = b + a;
+++a = b++;
+std::cout <<"the value of a is "<< a << std::endl;
+std::cout <<"the value of b is "<< b << std::endl;
 }
